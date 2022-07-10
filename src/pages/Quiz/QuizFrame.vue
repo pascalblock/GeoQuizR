d8a109f5952eeeb124eb319cd8a6ca47baf27c2d
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-07-10 12:38:38 +0200

    cleanup

--- src/pages/Quiz/QuizFrame.vue
+++ src/pages/Quiz/QuizFrame.vue
@@ -60,8 +60,6 @@ export default {
       actualQuestion: {},
       //url: 'https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png',
       //url: 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
-      ext: 'png',
-      // url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}',
       url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png',
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',      subdomains: 'abcd',
       minZoom: 4,
